f8415cc963f3facd25667268fe28e6c168cc3a53
Brian Coyle <bcoyle73@gmail.com>
2024-09-25 12:01:44 -0400

    basic homepage

---
+++ app/footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (
+      <footer className="bg-gray-800 text-white py-4 text-center">
+        &copy; 2024 Brian Coyle
+      </footer>
+    );
+  };
+  
+export default Footer;
